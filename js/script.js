cc216b34601ef4d97441e45e4eb01c379a93da00
sl24 <slvitalik24@gmail.com>
2020-09-27 18:47:39 +0300

    добавляет решение не обязательной части

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ function createGalleryMarkup(item) {
           alt="${description}"
         />
       </a>
-    </li> \n`;
+    </li> \n \n`;
     })
     .join('');
 }
@@ -57,6 +57,9 @@ function onGalleryImageClick(evt) {
   refs.modalWindow.classList.add('is-open'); // Открытие модального окна по клику на элементе галереи.
   refs.modalImage.src = evt.target.dataset.source; // Подмена значения атрибута src элемента img.lightbox__image.
   refs.modalImage.alt = evt.target.alt;
+
+  window.addEventListener('keydown', onEscapePress); // вешаем слушателя для отлова нажатия ктопнки Esc
+  window.addEventListener('keydown', onImageScroll);
 }
 
 /*
@@ -69,4 +72,69 @@ function onCloseModal() {
   refs.modalWindow.classList.remove('is-open');
   refs.modalImage.src = '';
   refs.modalImage.alt = '';
+
+  window.removeEventListener('keydown', onEscapePress); // удаляем слушателя кнопки Esc
+  window.removeEventListener('keydown', onImageScroll);
 }
+
+/*
+реализовываем закрытие модального окна по клику на оверлей
+*/
+
+refs.modalOverlay.addEventListener('click', onOverlayClick);
+
+function onOverlayClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    onCloseModal();
+  }
+}
+
+/*
+реализовываем закрытие модального окна по нажатию клавиши Escape
+*/
+
+function onEscapePress(evt) {
+  if (evt.code === 'Escape') {
+    onCloseModal();
+  }
+}
+
+/*
+листаем галерею влево-вправо
+*/
+
+function onImageScroll(evt) {
+  const LEFT_KEY_CODE = 'ArrowLeft';
+  const RIGHT_KEY_CODE = 'ArrowRight';
+
+  let getImageIndex = galleryItems.findIndex(
+    element => element.original === refs.modalImage.src,
+  );
+
+  if (evt.code === LEFT_KEY_CODE) {
+    if (getImageIndex === 0) {
+      onCloseModal(); // закрытие модального окна при нажатии влево при открытом ПЕРВОМ изображении
+      getImageIndex += 1; // чтобы не было ошибки в консолип при обращении к несуществующему индексу. Ничего лучше не придумал!!!
+      // return; // либо просто эта строка без закрытия модалки
+    }
+    getImageIndex -= 1;
+    // console.log(getImageIndex);
+  }
+
+  if (evt.code === RIGHT_KEY_CODE) {
+    if (getImageIndex === galleryItems.length - 1) {
+      onCloseModal(); // закрытие модального окна при нажатии вправо при открытом ПОСЛЕДНЕМ изображении
+      getImageIndex -= 1; // чтобы не было ошибки в консолип при обращении к несуществующему индексу. Ничего лучше не придумал!!!
+      // return; // либо просто эта строка без закрытия модалки
+    }
+    getImageIndex += 1;
+    // console.log(getImageIndex);
+  }
+
+  refs.modalImage.src = galleryItems[getImageIndex].original;
+  refs.modalImage.alt = galleryItems[getImageIndex].description;
+}
+
+// попробовал написать код закрытия модалки когда пробуешь пролистать крайние элементы
+// (например, листать влево, когда открыто первое изображение),
+// а листать уже нечего. не уверен правильное ли решение, но оно работает
